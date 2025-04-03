5e170f79e7698ffa7d09b494319b8636163e27dc
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2025-04-03 20:32:40 -0300

    Melhorei o código

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
      <div className="w-[500px">
-       <h1 className="text-3lx text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+       <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
         <AddTask />
         <Tasks  tasks={tasks}/>
      </div>
