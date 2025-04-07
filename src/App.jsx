a67068a56870543ab9e5f09284a1bf2a0290657a
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2025-04-06 21:52:11 -0300

    Continuando, modificando o tasks e deletando icones da interface

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,50 @@ import Tasks from "./components/Tasks";
 
 function App() {
   const [tasks, setTasks] = useState([
-  {
-    id: 1,
-    title: "Estudar programação",
-    description: "Estudar programação para se tornar um desenvolvedor full stack.",
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    title: "Estudar inglês",
-    description: "estudar inglês para se tornar fluente.",
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    title: "Estudar matemática",
-    description: "Estudar matemática para se tornar um desenvolvedor full stack.",
-    isCompleted: false,
-  },
-])
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "estudar inglês para se tornar fluente.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar matemática para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+  ])
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map(task => {
+      // PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return {...task, isCompleted: !task.isCompleted}
+      }
+
+      // NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-     <div className="w-[500px">
+     <div className="w-[500px]">
        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
         <AddTask />
-        <Tasks  tasks={tasks}/>
+        <Tasks  tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
      </div>
     </div>
   );
