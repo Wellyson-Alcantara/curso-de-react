36f4c301a188763ae716b2614b7a52b78f7dd51d
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2025-06-16 22:27:34 -0300

    tentando fazer deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import {v4} from "uuid";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import Title from "./components/title";
+import Title from "../components/Title";
 
 function App() {
   const [tasks, setTasks] = useState(
