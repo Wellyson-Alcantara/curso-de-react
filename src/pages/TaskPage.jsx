990ee4b9fcbc54a276b81ed3a3ad4bcec4633bfe
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2025-06-17 11:16:14 -0300

    Coloquei o check e consertei o display do Detalhes da Tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -8,7 +8,7 @@ function TaskPage() {
     const title = searchParams.get("title")
     const description = searchParams.get("description")
     return <div className="h-screen w-screen bg-slate-500 p-6">
-        <div className="w-[500px] mx-auto space-y-4">
+        <div className="w-auto mx-auto space-y-4">
             <div className="flex justify-center relative mb-6">
                 <button 
                     onClick={() => navigate(-1)} 
