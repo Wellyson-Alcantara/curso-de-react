44bf00dde250834c7b2a3cae4b0c64fdc7477a50
Antonio Wellyson Alcântara de Souza <146300371+Wellyson-Alcantara@users.noreply.github.com>
2025-06-16 22:29:59 -0300

    acho que resolvi o problema

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import {v4} from "uuid";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import Title from "../components/Title";
+import Title from "../components/title";
 
 function App() {
   const [tasks, setTasks] = useState(
